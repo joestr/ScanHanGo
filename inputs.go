78cad9be9b39516fea2ae5ed22b3ddda63a00c9f
Joel Strasser <strasser999@gmail.com>
2022-06-13 10:55:58 +0200

    adds mqtt to project

--- inputs.go
+++ inputs.go
@@ -4,13 +4,69 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
-type HandlerStruct struct {
-	name string
+type InputDevice struct {
+	name     string
+	phys     string
+	handlers []string
 }
 
-func GetHandlersRaw() string {
+func GetInputDevices() []InputDevice {
+	var result []InputDevice
+
+	var singleDevices []string = SplitIntoSingleDevicesRaw(GetInputDevicesRaw())
+
+	for i := range singleDevices {
+		result = append(result, ParseInputDevice(singleDevices[i]))
+	}
+
+	return result
+}
+
+func ParseInputDevice(device string) InputDevice {
+	var result InputDevice
+
+	regex := regexp.MustCompile("\n")
+	deviceLines := regex.Split(device, -1)
+
+	isNameSet := false
+	isPhysSet := false
+	areHandlersSet := false
+
+	for i := range deviceLines {
+		if !isNameSet {
+			val, success := ParseInputDeviceName(deviceLines[i])
+			if success {
+				result.name = val
+				isNameSet = true
+				continue
+			}
+		}
+		if !isPhysSet {
+			val, success := ParseInputDevicePhys(deviceLines[i])
+			if success {
+				result.phys = val
+				isPhysSet = true
+				continue
+			}
+		}
+		if !areHandlersSet {
+			val, success := ParseInputDeviceHandlers(deviceLines[i])
+			if success {
+				result.handlers = val
+				areHandlersSet = true
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+func GetInputDevicesRaw() string {
 	out, err := exec.Command("/bin/sh", "-c", "awk '/MARKB-[0-9]+ Keyboard/ {p=1} NF==0 {p=0}; p' /proc/bus/input/devices").Output()
 	if err != nil {
 		log.Fatal(err)
@@ -18,8 +74,54 @@ func GetHandlersRaw() string {
 	return fmt.Sprintf("%s\n", out)
 }
 
-func ParseHandlers() []HandlerStruct {
-	var result []HandlerStruct
+func SplitIntoSingleDevicesRaw(devices string) []string {
+	regex := regexp.MustCompile("^\n$")
 
-	return result
+	split := regex.Split(devices, -1)
+	var set []string
+
+	for i := range split {
+		set = append(set, split[i])
+	}
+
+	return set
+}
+
+func ParseInputDeviceName(line string) (string, bool) {
+	var result = ""
+
+	parts := strings.Split(line, "Name=")
+	if len(parts) < 2 {
+		return result, false
+	}
+
+	result = strings.Replace(parts[1], "\"", "", -1)
+
+	return result, true
+}
+
+func ParseInputDevicePhys(line string) (string, bool) {
+	var result = ""
+
+	parts := strings.Split(line, "Phys=")
+	if len(parts) < 2 {
+		return result, false
+	}
+
+	result = parts[1]
+
+	return result, true
+}
+
+func ParseInputDeviceHandlers(line string) ([]string, bool) {
+	var result []string
+
+	parts := strings.Split(line, "Handlers=")
+	if len(parts) < 2 {
+		return result, false
+	}
+
+	result = strings.Split(parts[1], " ")
+
+	return result, true
 }
--- main.go
+++ main.go
@@ -4,18 +4,67 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"os"
-	"time"
+	"strings"
 )
 
-func main() {
-	handlers := GetHandlersRaw()
-
-	fmt.Printf("%s\n", handlers)
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
-func main2() {
-	f, err := os.Open("/dev/input/event10")
+func main() {
+	inputDevices := GetInputDevices()
+
+	for i := range inputDevices {
+		fmt.Printf("Name=%s\n", inputDevices[i].name)
+		fmt.Printf("Phys=%s\n", inputDevices[i].phys)
+		for j := range inputDevices[i].handlers {
+			fmt.Printf("Handlers[%d]=%s\n", j, inputDevices[i].handlers[j])
+		}
+		fmt.Println("///   ///   ///")
+	}
+
+	opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipseprojects.io:1883")
+	opts.SetClientID("go-simple")
+	opts.SetDefaultPublishHandler(f)
+
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	for i := 0; i < 5; i++ {
+		text := fmt.Sprintf("this is msg #%d!", i)
+		token := c.Publish("go-mqtt/sample", 0, false, text)
+		token.Wait()
+	}
+
+	if len(inputDevices) > 0 {
+		for i := range inputDevices[0].handlers {
+			if strings.ContainsAny(inputDevices[0].handlers[i], "event") {
+				readLoop(inputDevices[0].handlers[i], inputDevices[0].phys)
+			}
+		}
+	}
+
+	if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
+}
+
+func readLoop(event string, physicalAddress string) {
+	var input []uint16
+	f, err := os.Open("/dev/input/" + event)
 	if err != nil {
 		panic(err)
 	}
@@ -23,14 +72,126 @@ func main2() {
 	b := make([]byte, 24)
 	for {
 		f.Read(b)
-		sec := binary.LittleEndian.Uint64(b[0:8])
-		usec := binary.LittleEndian.Uint64(b[8:16])
-		t := time.Unix(int64(sec), int64(usec))
-		fmt.Println(t)
+		//sec := binary.LittleEndian.Uint64(b[0:8])
+		//usec := binary.LittleEndian.Uint64(b[8:16])
+		//t := time.Unix(int64(sec), int64(usec))
+		//fmt.Println(t)
 		var value int32
 		typ := binary.LittleEndian.Uint16(b[16:18])
 		code := binary.LittleEndian.Uint16(b[18:20])
 		binary.Read(bytes.NewReader(b[20:]), binary.LittleEndian, &value)
-		fmt.Printf("type: %x\ncode: %d\nvalue: %d\n", typ, code, value)
+
+		if typ == 1 && value == 0 {
+			input = append(input, code)
+		}
+
+		if len(input) > 0 {
+			if input[len(input)-1] == 28 {
+				fmt.Printf("%s %s\n", physicalAddress, convertSequenceToString(input))
+				input = []uint16{}
+			}
+		}
+
+		//fmt.Printf("type: %x\ncode: %d\nvalue: %d\n", typ, code, value)
 	}
+}
+
+func convertSequenceToString(keycodeSequence []uint16) string {
+	var result string = ""
+
+	var uppercase bool = false
+	for i := range keycodeSequence {
+		var char string = ""
+
+		if keycodeSequence[i] == KEY_LEFTSHIFT {
+			uppercase = true
+			continue
+		}
+
+		switch keycodeSequence[i] {
+		case KEY_A:
+			char = "a"
+		case KEY_B:
+			char = "b"
+		case KEY_C:
+			char = "c"
+		case KEY_D:
+			char = "d"
+		case KEY_E:
+			char = "e"
+		case KEY_F:
+			char = "f"
+		case KEY_G:
+			char = "g"
+		case KEY_H:
+			char = "h"
+		case KEY_I:
+			char = "i"
+		case KEY_J:
+			char = "j"
+		case KEY_K:
+			char = "k"
+		case KEY_L:
+			char = "l"
+		case KEY_M:
+			char = "m"
+		case KEY_N:
+			char = "n"
+		case KEY_O:
+			char = "o"
+		case KEY_P:
+			char = "p"
+		case KEY_Q:
+			char = "q"
+		case KEY_R:
+			char = "r"
+		case KEY_S:
+			char = "s"
+		case KEY_T:
+			char = "t"
+		case KEY_U:
+			char = "u"
+		case KEY_V:
+			char = "v"
+		case KEY_W:
+			char = "w"
+		case KEY_X:
+			char = "x"
+		case KEY_Y:
+			char = "y"
+		case KEY_Z:
+			char = "z"
+		case KEY_1:
+			char = "1"
+		case KEY_2:
+			char = "2"
+		case KEY_3:
+			char = "3"
+		case KEY_4:
+			char = "4"
+		case KEY_5:
+			char = "5"
+		case KEY_6:
+			char = "6"
+		case KEY_7:
+			char = "7"
+		case KEY_8:
+			char = "8"
+		case KEY_9:
+			char = "9"
+		case KEY_0:
+			char = "0"
+		case KEY_MINUS:
+			char = "-"
+		}
+
+		if uppercase == true {
+			char = strings.ToUpper(char)
+			uppercase = false
+		}
+
+		result += char
+	}
+
+	return result
 }
